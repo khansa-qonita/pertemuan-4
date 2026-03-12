dacf688f5b63932141231438f7ef3455bc3076da
khansa-qonita <khansaqonita420@gmail.com>
2026-03-12 10:36:13 +0700

    menambahkan prosedur untuk input

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int arr[20];  //membuat array dengan panjang data 20
-int n;
-   
+int n;       // membuat variabel inputan n
+
+void input (){  //procedure input
+    while (true){
+        cout << "Masukkan Jumlah Data Paada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+
+}
+
