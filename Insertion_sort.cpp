5df65aacfbff5c3692c190f00d1b9bc028f9fc1a
khansa-qonita <khansaqonita420@gmail.com>
2026-03-12 10:40:10 +0700

    menambahkan procedure untuk input

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -16,6 +16,15 @@ void input (){  //procedure input
             cout << "nArray Tidak Boleh Lebih Dari 20\n";
         }
     }
+    cout << endl;
+    cout << "=====================================";
+    cout << "======Masukkan Element Array=========";
+    cout << "=====================================";
+
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
 
 }
 
